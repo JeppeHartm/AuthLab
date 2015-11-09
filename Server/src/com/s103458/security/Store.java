d5f03af6675d9ffa835a1afd002b7a3b5daa47bf
jeppe.hartmund@gmail.com <jeppe.hartmund@gmail.com>
2015-11-09 06:33:00 +0100

    almost working

--- Server/src/com/s103458/security/Store.java
+++ Server/src/com/s103458/security/Store.java
@@ -14,16 +14,15 @@ public class Store {
         }
     }
     public static void store(String name, String hashedkey) throws IOException {
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("resources/passwords")));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("passwords")));
         writer.write(name+":"+hashedkey);
         writer.newLine();
     }
     public static boolean lookup(String name,String hashedkey) throws IOException {
         BufferedReader reader = null;
         try {
-            reader = new BufferedReader(new InputStreamReader(new FileInputStream("resources/passwords")));
+            reader = new BufferedReader(new InputStreamReader(new FileInputStream("passwords")));
         }catch(FileNotFoundException fnfe){
-            reader = new BufferedReader(new InputStreamReader(new FileInputStream(new File("resources/passwords"))));
             return false;
         }
         String line;
